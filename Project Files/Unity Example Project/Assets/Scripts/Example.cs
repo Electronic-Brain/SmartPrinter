927fc1a27b90b2bee718e3d9b2f8992be09898cf
Electronic-Brain <46529413+Electronic-Brain@users.noreply.github.com>
2019-04-20 16:48:37 +0600

    v0.1
    Initial beta release of SmartPrinter. New features will be added if requested. Free to develop,name will be added on credit.

---
+++ Project Files/Unity Example Project/Assets/Scripts/Example.cs
@@ -0,0 +1,42 @@
+﻿/* This project may not be reproduced or selling anywhere without prior written permission of Electronic Brain.
+ * Project Developed by : Srejon Khan, Game Programmer, Electronic Brain and Ashikur Rahman, Game Programmer,Electronic Brain
+ * Email : support@electronicbrain.net 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SmartDLL;
+using UnityEngine.UI;
+
+public class Example : MonoBehaviour
+{
+    public SmartPrinter smartPrinter = new SmartPrinter();
+
+    public string headerDirectory;
+
+    public InputField textInput;
+    public Button printButton;
+
+
+
+    void OnEnable()
+    {
+        printButton.onClick.AddListener(delegate { PrintDocument(); });
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PrintDocument()
+    {
+        smartPrinter.PrintDocument(textInput.text, @headerDirectory);
+    }
+}
---
+++ Project Files/Visual Studio Project/SmartPrinter/Main.cs
@@ -0,0 +1,56 @@
+﻿/* This project may not be reproduced or selling anywhere without prior written permission of Electronic Brain.
+ * Project Developed by : Srejon Khan, Game Programmer, Electronic Brain and Ashikur Rahman, Game Programmer,Electronic Brain
+ * Email : support@electronicbrain.net 
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDLL
+{
+    public class SmartPrinter
+    {
+        #region variables
+        private Font printFont;
+        private string _texts;
+        private Bitmap header;
+        private string headerPath;
+        #endregion
+
+        /// <summary>
+        /// Call this function for Print a document with Header image. 
+        /// Header Image Resolution should be according to A4 page size. Download PSD templete from Github. 
+        /// A4 Page screen resolution : 595 pixels x 842 pixels 
+        /// </summary>
+        /// <param name="texts">String that you want to Print in doc. Use Environment.NewLine to break a line. Check Example Class for more info. </param>
+        /// <param name="headerDir">Header image dir with file name and extension.</param>
+        public void PrintDocument(string texts, string headerDir)
+        {
+            headerPath = headerDir; // Storing Path
+            _texts = texts; //Storing Header File Path
+            printFont = new Font("Arial", 12, FontStyle.Bold); //Font Name, Size and Font Style.
+            PrintDocument pd = new PrintDocument(); // Creating Object
+            pd.PrintPage += new PrintPageEventHandler(this.spPrint);
+            pd.Print();
+        }
+
+        /// <summary>
+        /// Page Settings and Intialization
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void spPrint(object sender, PrintPageEventArgs e)
+        {
+            float leftMargin = e.MarginBounds.Left; //Left Margin of Page
+            float topMargin = e.MarginBounds.Top; // Top Margin of Page
+
+            Image image = Image.FromFile(headerPath); // Streaming image from File. File must be local.
+            e.Graphics.DrawImage(image, (e.PageBounds.Width - image.Width) / 2, e.PageBounds.Y, image.Width, image.Height); //Drawing Header.
+            e.Graphics.DrawString(_texts, printFont, Brushes.Black, new Point(25, 150));// Drawing Texts
+        }
+    }
+}
